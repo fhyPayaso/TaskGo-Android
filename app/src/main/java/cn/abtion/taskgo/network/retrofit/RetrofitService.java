f9f0e631e88449027056cc66456ed6ebc93449c0
Ajiajiajia <549044363@qq.com>
2018-02-02 17:04:06 +0800

    third

--- app/src/main/java/cn/abtion/taskgo/network/retrofit/RetrofitService.java
+++ app/src/main/java/cn/abtion/taskgo/network/retrofit/RetrofitService.java
@@ -2,8 +2,9 @@ package cn.abtion.taskgo.network.retrofit;
 
 
 import cn.abtion.taskgo.mvp.model.request.account.LoginRequestModel;
+import cn.abtion.taskgo.mvp.model.request.account.RegisterRequestModel;
+import cn.abtion.taskgo.mvp.model.request.account.UpdatePasswordRequestModel;
 import cn.abtion.taskgo.network.response.ApiResponse;
-import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
@@ -17,14 +18,6 @@ import retrofit2.http.POST;
 public interface RetrofitService {
 
 
-    /**
-     * 登录接口
-     * @param loginRequest
-     * @return
-     */
-    @POST("login")
-    Observable<ApiResponse> rxLogin(@Body LoginRequestModel loginRequest);
-
 
 
     /**
@@ -34,5 +27,28 @@ public interface RetrofitService {
      */
     @POST("login")
     Call<ApiResponse> login(@Body LoginRequestModel loginRequest);
+
+
+    @POST("user/register")
+    Call<APIResponse> register(@Body RegisterRequestModel mregisterRequestModel);
+
+    /**
+     * 登录
+     * 手机号登录     获取 token 以及 user_id
+     *  微信/QQ登录   验证完后输入微信或QQ号登录 获取token以及user_id
+     */
+    @POST("user/login")
+    Call<APIResponse> rxlogin(@Body LoginRequestModel mloginRequestModel);
+
+    /**
+     * 忘记密码（修改密码）
+     *
+     */
+    @POST("user/resetPassword")
+    Call<APIResponse> updatepasswordrequestmodel(@Body UpdatePasswordRequestModel mupdatePasswordRequestModel);
+
+
+
+
 
 }
